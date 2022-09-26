329377283c6a2df05c61cc37dc9f96e6df10881f
NicolaSpadari <spadari.nicola@gmail.com>
2022-09-26 22:02:11 +0200

    Test to fix import meta

--- src/composables/useMessage.ts
+++ src/composables/useMessage.ts
@@ -1,9 +1,12 @@
+const TELEGRAM_BOT_TOKEN = import.meta.env.VITE_TELEGRAM_BOT_TOKEN;
+const TELEGRAM_GROUP_ID = import.meta.env.VITE_TELEGRAM_GROUP_ID;
+
 const useMessage = () => {
     const sendMessage = (msg: string) => {
-        const BOT_URL = `https://api.telegram.org/bot${import.meta.env.VITE_TELEGRAM_BOT_TOKEN}`;
+        const BOT_URL = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}`;
         msg = encodeURIComponent(msg);
 
-        useFetch(`${BOT_URL}/sendMessage?chat_id=${import.meta.env.VITE_TELEGRAM_GROUP_ID}&text=${msg}`).post();
+        useFetch(`${BOT_URL}/sendMessage?chat_id=${TELEGRAM_GROUP_ID}&text=${msg}`).post();
     };
 
     return {
